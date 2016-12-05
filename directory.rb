a52e97828a46f294190d5911e9d3197ed9ecdede
Ben Kielty <bwk103@gmail.com>
2016-12-05 10:59:50 +0000

    Refactoring directory.rb - adding methods

--- directory.rb
+++ directory.rb
@@ -11,10 +11,21 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"]
 
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-" * 13
 end
 
-puts "Overall, we have #{students.count} great students"
+def print_names(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print_names(students)
+print_footer(students)
