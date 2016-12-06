b2b09a4f1b4b43e66dacbbb7c4363f2ade505d88
Ben Kielty <bwk103@gmail.com>
2016-12-06 12:19:14 +0000

    Refactoring interactive menu

--- directory.rb
+++ directory.rb
@@ -1,8 +1,8 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
-  students = []
 
   poss_cohorts = ["january", "february", "march", "april", "may", "june",
             "july", "august", "september", "october", "november", "december"]
@@ -26,43 +26,50 @@ def input_students
     country = gets.delete("\n")
     puts "What is #{name}'s height?"
     height = gets.delete("\n")
-    students << {name: name, cohort: cohort, hobbies: [hobbies.split(", ")], country_of_birth: country, height: height}
-    if students.length == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort, hobbies: [hobbies.split(", ")], country_of_birth: country, height: height}
+    if @students.length == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     name = gets.delete("\n")
   end
-  if students.length < 1
+  if @students.length < 1
     puts "You've not provided any students!"
     exit
-  else
-    students
   end
 end
 
 def interactive_menu
-students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Display the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print_names(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
       puts "I don't know what you meant, try again"
-    end
   end
+end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Display the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -71,39 +78,39 @@ def print_header
   puts ("-" * 13).center(80)
 end
 
-def print_names(students)
+def print_students_list
   count = 0
-  while count != students.length
-    puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]})".center(80)
+  while count != @students.length
+    puts "#{count + 1}. #{@students[count][:name]} (#{@students[count][:cohort]})".center(80)
     count += 1
   end
 end
 
-def names_with_specific_letter(students)
+def names_with_specific_letter
   puts "To search for individual students,"
   puts "please provide the first letter of their name"
   letter = gets.delete("\n").upcase
   puts ("The students with names beginning with the letter #{letter} are:").center(80)
-  students. each do |student|
+  @students.each do |student|
     puts (student[:name]).center(80) if student[:name][0] == letter
   end
 end
 
-def print_short_names(students)
+def print_short_names
   puts "The students with names shorter than 12 characters are:"
-  students.each do |student|
+  @students.each do |student|
     (puts student[:name] if student[:name].length < 12)
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(80)
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(80)
 end
 
-def print_by_cohort(students)
+def print_by_cohort
   puts "To search for students by cohort, please provide a cohort (month)"
   cohort = gets.delete("\n").to_sym
-  students.each do |student|
+  @students.each do |student|
     puts (student[:name]).center(80) if student[:cohort] == cohort
   end
 end
