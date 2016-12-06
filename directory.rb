c1afb75ac69fbff705e88a97c014a736fe54539e
Ben Kielty <bwk103@gmail.com>
2016-12-06 12:33:47 +0000

    Adding save to file functionality

--- directory.rb
+++ directory.rb
@@ -53,6 +53,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -63,6 +65,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Display the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -70,6 +73,18 @@ def show_students
   print_header
   print_students_list
   print_footer
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def print_header
