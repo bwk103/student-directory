e7884787173462e11424eeddd1c180359fb87f95
Ben Kielty <bwk103@gmail.com>
2016-12-08 14:53:10 +0000

    Adding 'delete student' functionality

--- directory.rb
+++ directory.rb
@@ -39,11 +39,13 @@ def process(selection)
     when "6"
       search_by_cohort
     when "7"
-      save_students
+      delete_student
     when "8"
+      save_students
+    when "9"
       filename = load_info
       load_students(filename)
-    when "9"
+    when "10"
       exit
     else
       puts "I don't know what you meant, try again"
@@ -55,15 +57,16 @@ def print_menu
   puts "-" * menu_string.length
   puts "Please select one of the following options:"
   puts
-  puts "1. Input the students"
-  puts "2. Display all students"
-  puts "3. Show full information for individual student"
-  puts "4. Search students by name"
-  puts "5. Search students by length of name"
-  puts "6. Search students by cohort"
-  puts "7. Save a list"
-  puts "8. Load a list"
-  puts "9. Exit"
+  puts "1.  Input the students"
+  puts "2.  Display all students"
+  puts "3.  Show full information for individual student"
+  puts "4.  Search students by name"
+  puts "5.  Search students by length of name"
+  puts "6.  Search students by cohort"
+  puts "7.  Delete student from list"
+  puts "8.  Save a list"
+  puts "9.  Load a list"
+  puts "10. Exit"
 end
 
  ### INPUTTING STUDENT INFORMATION
@@ -140,27 +143,28 @@ end
 ## DISPLAY LIST OF STUDENTS
 
 def show_students
-  print_header
+  print_header("The Students of Villains Academy:")
   print_students_list
   print_footer
 end
 
-def print_header
-  puts ("-" * 13).center(80)
-  puts "The students of Villains Academy".center(80)
-  puts ("-" * 13).center(80)
+def print_header(phrase)
+  puts "-" * phrase.length
+  puts phrase
+  puts "-" * phrase.length
 end
 
 def print_students_list
   count = 0
   while count != @students.length
-    puts "#{count + 1}. #{@students[count][:name]} (#{@students[count][:cohort]})".center(80)
+    puts "#{count + 1}. #{@students[count][:name]} (#{@students[count][:cohort]})"
     count += 1
   end
 end
 
 def print_footer
-  puts "Overall, we have #{@students.count} great students".center(80)
+  puts
+  puts "Overall, we have #{@students.count} great students"
 end
 
 
@@ -170,10 +174,9 @@ def print_one_student
   if @students.length == 0
     no_students
   else
+    print_header("Please select a student from the current list:")
     print_students_list
     puts
-    puts "Please select a student in order to see their full information,"
-    puts "including their hobbies, country of birth and height."
     selection = STDIN.gets.chomp.to_i
     idx = selection - 1
     puts
@@ -196,12 +199,12 @@ def search_by_letter
   if @students.length == 0
     no_students
   else
-    puts "To search for students by name,"
-    puts "please provide the letter that their first name starts with"
+    count = 0
+    print_header("Please provide a letter...")
     letter = STDIN.gets.delete("\n").upcase
-    puts ("The students with names beginning with the letter #{letter} are:").center(80)
+    print_header("The students with names beginning with the letter '#{letter}' are:")
     @students.each do |student|
-      puts (student[:name]).center(80) if student[:name][0] == letter
+      puts "- #{student[:name]}, Cohort: #{student[:cohort]}" if student[:name][0] == letter
     end
   end
 end
@@ -210,13 +213,12 @@ def search_by_length
   if @students.length == 0
     no_students
   else
-    puts "What is the maximum length of characters in the student's name?"
+    print_header("What is the maximum length of characters in the student's name?")
     choice = STDIN.gets.chomp.to_i
     puts
-    puts "These are the students whose names are #{choice} characters, or less:"
-    puts
+    print_header("These are the students whose names are #{choice} characters, or less:")
     @students.each do |student|
-      puts "- " + student[:name] if student[:name].length <= choice
+      puts "- #{student[:name]}" if student[:name].length <= choice
     end
   end
 end
@@ -225,11 +227,11 @@ def search_by_cohort
   if @students.length == 0
     no_students
   else
-    puts "To search for students by cohort, please provide a cohort (month)"
+    print_header("To search for students by cohort, please provide a cohort (month)")
     cohort = STDIN.gets.delete("\n")
-    puts "The students in the #{cohort} cohort are:"
+    print_header("The students in the #{cohort} cohort are:")
     @students.each do |student|
-      puts (student[:name]).center(80) if student[:cohort] == cohort
+      puts "- #{student[:name]}" if student[:cohort] == cohort
     end
   end
 end
@@ -239,6 +241,34 @@ def no_students
   puts "Try adding student details using option '1' or loading a student list"
   puts "using option '7'."
 end
+
+
+## DELETE STUDENT
+
+def delete_student
+  if @students.length == 0
+    no_students
+  else
+    print_students_list
+    puts
+    puts "Which number student do you wish to delete?"
+    choice = STDIN.gets.chomp.to_i
+    idx = choice - 1
+    puts "Are you sure that you want to delete #{@students[idx][:name]}? Y/N"
+    selection = STDIN.gets.chomp.upcase
+    while selection
+      if selection == "Y"
+        @students.delete_at(idx)
+        break
+      elsif selection == "N"
+        break
+      else
+        puts "I'm sorry, I didn't quite catch that."
+      end
+    end
+  end
+end
+
 
 ## SAVE STUDENT LIST
 
